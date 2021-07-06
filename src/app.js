86e255fc1269f6ebecda6ed1efde39dd7e4bd2d3
RichiCorrea <richi.correa.f@gmail.com>
2021-07-06 15:14:41 +0000

    oki doki

--- src/app.js
+++ src/app.js
@@ -2,135 +2,207 @@
 import "bootstrap";
 import "./style.css";
 
-let cardsArray = new Array();
-let drawCardsButton = document.getElementById("draw-button");
-let sortCardsButton = document.getElementById("sort-button");
-let suitsCard = ["♠", "♦", "♥", "♣"];
-let numsCard = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
 
-//Class card
-class Card {
-  constructor(suit, num) {
-    this.suit = suit;
-    this.num = num;
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+let simbolos = ["♥", "♦", "♠", "♣"];
+
+//seleccionar cointarnerglobar
+let global = document.querySelector("#super");
+
+//tomar boton draw
+let botonDraw = document.querySelector("#generar");
+
+//tomar boton sort
+
+let btnSort = document.querySelector("#sort");
+
+//Tomar el input original
+let inputOr = document.querySelector("#original");
+
+//Llamar boton clear
+let btnClear = document.querySelector("#clear");
+
+//lamar boton sort
+let btnSelection = document.querySelector("#selecction");
+//carta aleatoria
+function cartaAleatoria() {
+  return numeros[Math.floor(Math.random() * 13)];
+}
+//simbolo aleatorio
+function simboloAleatorio() {
+  return simbolos[Math.floor(Math.random() * 4)];
+}
+
+//Arreglo que guarda el draw
+let arregloDraw = [];
+
+window.onload = function() {
+  botonDraw.addEventListener("click", () => {
+    imprimirDraws(generarArreglo(inputOr.value));
+  });
+
+  btnClear.addEventListener("click", () => {
+    clear();
+  });
+
+  btnSort.addEventListener("click", () => {
+    ordenarImprimir();
+  });
+
+  btnSelection.addEventListener("click", () => {
+    selectionalgori();
+  });
+};
+
+function draw(valor, simbolo) {
+  let contenedorDraw = document.querySelector("#draw");
+  // let simbolo = simboloAleatorio();
+  if (valor == 11) {
+    valor = "J";
+  } else if (valor == 12) {
+    valor = "Q";
+  } else if (valor == 13) {
+    valor = "K";
+  }
+  let clase = "black";
+  if (simbolo == "♥" || simbolo == "♦") {
+    clase = "red";
+  }
+
+  contenedorDraw.innerHTML += `<div class="card ${clase}">
+          <p class="arriba">${simbolo}</p>
+          <p class="medio">${valor}</p>
+          <p class="abajo">${simbolo}</p>
+        </div>`;
+}
+function drawBubble(valor, simbolo) {
+  let contenedorG = document.querySelector("#generadas");
+  // let simbolo = simboloAleatorio();
+  if (valor == 11) {
+    valor = "J";
+  } else if (valor == 12) {
+    valor = "Q";
+  } else if (valor == 13) {
+    valor = "K";
+  }
+  let clase = "black";
+  if (simbolo == "♥" || simbolo == "♦") {
+    clase = "red";
+  }
+  let nuegog = document.createElement("DIV");
+  global.appendChild(nuegog);
+  nuegog.innerHTML += `<div class="card ${clase} d-flex">
+          <p class="arriba">${simbolo}</p>
+          <p class="medio">${valor}</p>
+          <p class="abajo">${simbolo}</p>
+        </div>`;
+}
+
+//funcion que toma el length de original e imprime los draw
+function imprimirDraws(arreglo) {
+  // for (let i in arreglo) {
+  //   draw(arreglo[i]);
+  //   console.log(arreglo[i]);
+  // }
+  for (let i = 0; i < arregloDraw.length; i++) {
+    let a = arregloDraw[i][0];
+    let b = arregloDraw[i][1];
+    draw(a, b);
   }
 }
 
-drawCardsButton.addEventListener("click", function() {
-  clearSortContainer();
-  let numCards = document.getElementById("inputCards").value;
-  cardsArray = generateCardsArray(numCards); //cardsArray
-  addDOMCards(cardsArray, false, 0); //add cards to the document
-});
+function imprimirBubbles(arreglo) {
+  //creo div y lo agrego
+  let nuegog = document.createElement("DIV");
+  nuegog.classList.add("d-flex");
+  global.appendChild(nuegog);
 
-sortCardsButton.addEventListener("click", function() {
-  clearSortContainer();
-  bubbleSort(cardsArray);
-});
+  for (let i = 0; i < arregloDraw.length; i++) {
+    let a = arregloDraw[i][0];
+    let b = arregloDraw[i][1];
+    if (a == 11) {
+      a = "J";
+    } else if (a == 12) {
+      a = "Q";
+    } else if (a == 13) {
+      a = "K";
+    }
+    let clase = "black";
+    if (b == "♥" || b == "♦") {
+      clase = "red";
+    }
 
-function clearSortContainer() {
-  let cardsContainerSort = document.getElementById("cardsContainerSort");
-  while (cardsContainerSort.firstChild) {
-    cardsContainerSort.removeChild(cardsContainerSort.firstChild);
+    nuegog.innerHTML += `<div class="card ${clase} mb-2 a">
+          <p class="arriba">${b}</p>
+          <p class="medio">${a}</p>
+          <p class="abajo">${b}</p>
+        </div>`;
   }
 }
 
-function addDOMCards(cardsArray, sort, numIteration) {
-  let cardsContainer = document.getElementById("cardsContainer");
-  let cardsContainerSort = document.getElementById("cardsContainerSort");
-  let cardsDOM = "";
-  for (let i = 0; i < cardsArray.length; i++) {
-    cardsDOM += generateDOMCard(cardsArray[i]);
+//funcion generar arreglo
+function generarArreglo(origin) {
+  for (let i = 0; i < origin; i++) {
+    let miniarr = [cartaAleatoria(), simboloAleatorio()];
+    arregloDraw.push(miniarr);
   }
-  if (!sort) {
-    cardsContainer.innerHTML =
-      '<div class="cards-container-row">' + cardsDOM + "</div>";
-  } else {
-    cardsContainerSort.innerHTML +=
-      '<div class="cards-container-sort-row"> <div class="iteration-num-container"> <p class="iteration-num">' +
-      numIteration +
-      "</p> </div> " +
-      cardsDOM +
-      " </div>";
-  }
+  console.log(arregloDraw);
+  return arregloDraw;
 }
 
-function generateDOMCard(card) {
-  if (["♦", "♥"].includes(card.suit)) {
-    return (
-      '<div class="card"> <div class="container-suit-top suit"> <p class="redColor">' +
-      card.suit +
-      '</p> </div> <div class="container-number"> <p>' +
-      card.num +
-      '</p> </div> <div class="container-suit-button suit"> <p class="redColor">' +
-      card.suit +
-      "</p> </div> </div>"
-    );
-  } else {
-    return (
-      '<div class="card"> <div class="container-suit-top suit"> <p class="blackColor">' +
-      card.suit +
-      '</p> </div> <div class="container-number"> <p>' +
-      card.num +
-      '</p> </div> <div class="container-suit-button suit"> <p class="blackColor">' +
-      card.suit +
-      "</p> </div> </div>"
-    );
+//EXTRA funcion clear
+
+function clear() {
+  let contenedorDraw = document.querySelector("#draw");
+  arregloDraw = [];
+  contenedorDraw.innerHTML = "<div></div>";
+  inputOr.value = "";
+  let items = document.querySelectorAll(".a");
+  for (let item of items) {
+    item.parentNode.remove();
   }
 }
 
-function generateCardsArray(numCards) {
-  let cardsArray = new Array();
-
-  for (let i = 0; i < numCards; i++) {
-    let card = new Card();
-    card.suit = suitsCard[getRndInteger(0, suitsCard.length - 1)];
-    card.num = numsCard[getRndInteger(0, numsCard.length - 1)];
-    cardsArray.push(card);
-  }
-  return cardsArray;
-}
-
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const bubbleSort = cardsArray => {
-  let newCardArray = cardsArray.slice();
-  let wall = newCardArray.length - 1;
-  let numIteration = -1;
+function ordenarImprimir() {
+  let wall = arregloDraw.length - 1;
   while (wall > 0) {
     let index = 0;
     while (index < wall) {
-      if (
-        numsCard.indexOf(newCardArray[index].num) >
-        numsCard.indexOf(newCardArray[index + 1].num)
-      ) {
-        let aux = newCardArray[index];
-        newCardArray[index] = newCardArray[index + 1];
-        newCardArray[index + 1] = aux;
-        numIteration++;
-        addDOMCards(newCardArray, true, numIteration);
+      if (arregloDraw[index][0] > arregloDraw[index + 1][0]) {
+        let aux = arregloDraw[index][0];
+        let auxS = arregloDraw[index][1];
+        arregloDraw[index][0] = arregloDraw[index + 1][0];
+        arregloDraw[index][1] = arregloDraw[index + 1][1];
+        arregloDraw[index + 1][0] = aux;
+        arregloDraw[index + 1][1] = auxS;
+        imprimirBubbles();
       }
       index++;
     }
+
     wall--;
   }
-  if (numIteration == -1) {
-    addDOMCards(newCardArray, true, 0);
+  return arregloDraw;
+}
+
+function selectionalgori() {
+  let min = 0;
+  while (min < arregloDraw.length - 1) {
+    for (let i = min + 1; i < arregloDraw.length; i++) {
+      if (arregloDraw[min][0] > arregloDraw[i][0]) {
+        let aux = arregloDraw[min][0];
+        let auxs = arregloDraw[min][1];
+        arregloDraw[min][0] = arregloDraw[i][0];
+        arregloDraw[min][1] = arregloDraw[i][1];
+        arregloDraw[i][0] = aux;
+        arregloDraw[i][1] = auxs;
+        imprimirBubbles();
+        console.log(arregloDraw);
+      }
+    }
+    min++;
   }
-};
+  return arregloDraw;
+}
